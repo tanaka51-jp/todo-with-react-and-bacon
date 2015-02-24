59dc417e739016348bb7a6c89b8608e52a1012d6
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-02-25 02:15:45 +0900

    Add limitation to todos table

--- db/migrate/20150224171421_create_todos.rb
+++ db/migrate/20150224171421_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-      t.string :name
-      t.boolean :done
+      t.string :name, null: false
+      t.boolean :done, null: false, default: false
 
       t.timestamps null: false
     end
